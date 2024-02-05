6ec4448c663a0b041a0016db99339b4234d9ef46
h.efnane <h.efnane@idealsoft.fr>
2024-02-05 12:05:51 +0100

    poshog

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import posthog from 'posthog-js'
+posthog.init(phc_KVwMPH1zpeKPIIYKsCKqoA5LwEyeaUYxwHXzT92aGeX, { api_host: 'https://app.posthog.com' })
+posthog.capture('$pageview')
+
 let serieA = document.querySelector("#serieA");
 let coppaIta = document.querySelector("#coppaIta");
 let supercoppa = document.querySelector("#supercoppaIta");
