872acf9882bc659df20e8430a6984e6153aeab08
h.efnane <h.efnane@idealsoft.fr>
2024-02-05 11:24:10 +0100

    tomotmo

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-import posthog from 'posthog-js'
-posthog.init(phc_KVwMPH1zpeKPIIYKsCKqoA5LwEyeaUYxwHXzT92aGeX, { api_host: 'https://app.posthog.com' })
-posthog.capture('$pageview')
-
 let serieA = document.querySelector("#serieA");
 let coppaIta = document.querySelector("#coppaIta");
 let supercoppa = document.querySelector("#supercoppaIta");
